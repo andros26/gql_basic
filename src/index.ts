349b2f47cd7a4e872395d936ff9b2e40a0344fa9
andros26 <108378283+andros26@users.noreply.github.com>
2025-11-21 07:55:15 -0500

    run app in docker

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { schema } from "./graphql";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 
-const app = express()
-app.use(cors())
+const app: Application = express();  // <--- Cambiado a Application
+app.use(cors());
+
 const server = new ApolloServer({
     schema,
     introspection: true,
     plugins: [
         ApolloServerPluginLandingPageGraphQLPlayground()
     ]
-})
-
+});
 
 server.start().then(res => {
     server.applyMiddleware({ app });
 
     app.listen({ port: 4000 }, () => {
-        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-    })
+        console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 4000}${server.graphqlPath}`);
+    });
+
 })
